6af246e759daa04cfe1957efc5a2c0c775ce3a9b
Zakkaray <bloomzakk@gmail.com>
2020-11-28 00:30:45 +0200

    [Component decomposition] imported all files

---
+++ helpers/DefaultPageWrapper.jsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <meta charSet="UTF-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Oleo+Script&family=Raleway:wght@700&family=Roboto:wght@400;500;700;900&display=swap"
+                    rel="stylesheet"
+                />
+                <link
+                    rel="stylesheet"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/1.0.0/modern-normalize.min.css"
+                />
+                <link rel="stylesheet" href="./css/stylesIndex.css"/>
+                <title>Queuelab</title>
+            </Head>
+
+        </div>
+    )
+}
